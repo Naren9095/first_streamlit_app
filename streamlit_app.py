8f99227a3d10fa99654f34396d0b38ebfba2c711
Naren9095 <120551803+Naren9095@users.noreply.github.com>
2023-05-06 18:11:41 +0530

    Update streamlit_app.py
    added some extra changes in Mneu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
 
 streamlit.title("My Parents favourite Diner")
+
 streamlit.header("Breakfast")
-streamlit.text("Idly")
-streamlit.text("Dosa")
-streamlit.text("Pongal")
+streamlit.text("🥣 Idly")
+streamlit.text("🥗 Dosa")
+streamlit.text("🥑🍞 Pongal")
+streamlit.text("🐔 Boiled Egg")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
