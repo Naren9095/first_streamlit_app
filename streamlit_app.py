232ae08010d965087a91aa90fe8e91f0f153bede
Naren9095 <120551803+Naren9095@users.noreply.github.com>
2023-05-07 23:17:46 +0530

    Update streamlit_app.py
    normalizing the JSON data from API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("FruityVice Advice! Check this once")
+
 import requests
+# Making an API call to get the data
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Using Pandas function to normalize the Json data, so the user can understand it easily
+FruityVice_Normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(FruityVice_Normalized)
 
 
 
