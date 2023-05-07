d46e69afcc57c271aedda79329ca02244d74cad4
Naren9095 <120551803+Naren9095@users.noreply.github.com>
2023-05-07 23:10:39 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("FruityVice Advice! Check this once")
 import request
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text()fruityvice_response.json)
+streamlit.text(fruityvice_response.json)
 
 
 
